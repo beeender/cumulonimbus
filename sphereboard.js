720999602bca3c95b44015453bbc55b1609a96a6
Chen Mulong <chenmulong@gmail.com>
2013-02-23 15:57:51 +0800

    Lint errors fix.

--- sphereboard.js
+++ sphereboard.js
@@ -1,76 +1,89 @@
+/*global TagBoard*/
+
 var SphereBoard = {
-    createNew: function(canvas, context, sprites, opts) {
-        var sphereBoard = TagBoard.createNew(canvas, context, sprites, opts);
+    createNew: function (canvas, context, sprites, opts) {
+        "use strict";
+        var sphereBoard = TagBoard.createNew(canvas, context, sprites, opts),
+            inc,
+            off,
+            i,
+            x,
+            y,
+            z,
+            r,
+            phi,
+            super_Draw = sphereBoard.draw,
+            super_OnMouseMove = sphereBoard.onMouseMove;
 
         //Leave some space for text rendering.
-        sphereBoard.radius = Math.min(sphereBoard.width/2, sphereBoard.height/2) * 0.8;
-
+        sphereBoard.radius = Math.min(sphereBoard.width / 2, sphereBoard.height / 2) * 0.8;
         //Perfectly evenly distributed points on a sphere
         //http://www.softimageblog.com/archives/115
         inc = 3.14 * (3 - Math.sqrt(5));
-        off = 2/sphereBoard.sprites.length;
-        for(i=0; i<sphereBoard.sprites.length; i++){ 
-            y = i * off - 1 + (off/2);
-            r = Math.sqrt(1 - y*y);
+        off = 2 / sphereBoard.sprites.length;
+
+        for (i = 0; i < sphereBoard.sprites.length; i += 1) {
+            y = i * off - 1 + (off / 2);
+            r = Math.sqrt(1 - y * y);
             phi = i * inc;
             x = Math.cos(phi) * r;
             z = Math.sin(phi) * r;
 
-            sphereBoard.sprites[i].x = x * sphereBoard.radius + sphereBoard.width/2;
-            sphereBoard.sprites[i].y = y * sphereBoard.radius + sphereBoard.height/2;
+            sphereBoard.sprites[i].x = x * sphereBoard.radius + sphereBoard.width / 2;
+            sphereBoard.sprites[i].y = y * sphereBoard.radius + sphereBoard.height / 2;
             sphereBoard.sprites[i].z = z * sphereBoard.radius;
         }
 
         sphereBoard.speed_x = 1;
         sphereBoard.speed_y = 1;
-        sphereBoard.rotation_y = Math.PI/45.0 * sphereBoard.speed_y;
-        sphereBoard.rotation_x = Math.PI/45.0 * sphereBoard.speed_x;
+        sphereBoard.rotation_y = Math.PI / 45.0 * sphereBoard.speed_y;
+        sphereBoard.rotation_x = Math.PI / 45.0 * sphereBoard.speed_x;
 
         sphereBoard.f_size_max = 17;
         sphereBoard.f_size_min = 5;
 
         sphereBoard.sortSprites('z', true);
 
-        super_Draw = sphereBoard.draw;
-        sphereBoard.draw = function() { 
-            var found_focus = false;
-            var fast_draw = false;
+        sphereBoard.draw = function () {
+            var found_focus = false,
+                fast_draw = false,
+                spr,
+                font_size;
 
-            if(Math.abs(sphereBoard.rotation_x) <= 0.01 && Math.abs(sphereBoard.rotation_y) <= 0.01) { 
+            if (Math.abs(sphereBoard.rotation_x) <= 0.01 && Math.abs(sphereBoard.rotation_y) <= 0.01) {
                 return;
             }
 
             //Draw background
             super_Draw();
 
-            for(i = 0; i < sphereBoard.sprites.length; i++) { 
-                var spr = sphereBoard.sprites[i];
-                var font_size;
+            for (i = 0; i < sphereBoard.sprites.length; i += 1) {
+                spr = sphereBoard.sprites[i];
 
-                if(spr.z < sphereBoard.radius*2/3 ) { 
+                if (spr.z < sphereBoard.radius * 2 / 3) {
                     font_size = sphereBoard.f_size_min;
-                } else { 
-                    font_size = sphereBoard.f_size_max * Math.abs(spr.z)/sphereBoard.radius;
-                    if(font_size === 0) { 
+                } else {
+                    font_size = sphereBoard.f_size_max * Math.abs(spr.z) / sphereBoard.radius;
+                    if (font_size === 0) {
                         font_size = sphereBoard.f_size_min;
                     }
 
-                    if(sphereBoard.mouseOnSprite(spr, font_size)) {
+                    if (sphereBoard.mouseOnSprite(spr, font_size)) {
                         //Sprites is sorted! Set the top one as focused!
-                        if(!found_focus) { 
+                        if (!found_focus) {
                             sphereBoard.focusspr = spr;
                             found_focus = true;
                         }
                     }
                 }
 
-                if(sphereBoard.focusspr == spr) { 
+                if (sphereBoard.focusspr === spr) {
                     sphereBoard.drawSprite(spr, Math.round(font_size), "#333333", true, fast_draw);
-                } else { 
+                } else {
                     sphereBoard.drawSprite(spr, Math.round(font_size), "#333333", false, fast_draw);
                 }
 
-                if(spr.z < sphereBoard.radius*2/3 ) { 
+                if (spr.z < sphereBoard.radius * 2 / 3) {
                     //The sprites is sorted. Use fast draw!
                     fast_draw = true;
                 }
@@ -83,37 +96,31 @@ var SphereBoard = {
             sphereBoard.sortSprites('z', true);
         };
 
-        sphereBoard.spritesMove = function() { 
-            sphereBoard.rotation_y = Math.PI/45.0 * sphereBoard.speed_y;
-            sphereBoard.rotation_x = Math.PI/45.0 * sphereBoard.speed_x;
+        sphereBoard.spritesMove = function () {
+            sphereBoard.rotation_y = Math.PI / 45.0 * sphereBoard.speed_y;
+            sphereBoard.rotation_x = Math.PI / 45.0 * sphereBoard.speed_x;
 
             sphereBoard.speed_x *= 0.97;
             sphereBoard.speed_y *= 0.97;
         };
 
-        super_OnMouseMove = sphereBoard.onMouseMove;
-        sphereBoard.onMouseMove = function(x, y) {
-            var center_x = sphereBoard.width/2;
-            var center_y = sphereBoard.height/2;
-            var a = x - center_x;
-            var b = y - center_y;
+        sphereBoard.onMouseMove = function (x, y) {
+            var center_x = sphereBoard.width / 2,
+                center_y = sphereBoard.height / 2,
+                a = x - center_x,
+                b = y - center_y;
 
-            sphereBoard.speed_x = b/sphereBoard.radius;
-            sphereBoard.speed_y = a/sphereBoard.radius;
-            if(sphereBoard.speed_x > 1) 
-            { 
+            sphereBoard.speed_x = b / sphereBoard.radius;
+            sphereBoard.speed_y = a / sphereBoard.radius;
+            if (sphereBoard.speed_x > 1) {
                 sphereBoard.speed_x = 1;
-            }
-            else if(sphereBoard.speed_x < -1)
-            { 
+            } else if (sphereBoard.speed_x < -1) {
                 sphereBoard.speed_x = -1;
             }
-            if(sphereBoard.speed_y > 1) 
-            { 
+
+            if (sphereBoard.speed_y > 1) {
                 sphereBoard.speed_y = 1;
-            }
-            else if(sphereBoard.speed_y < -1)
-            { 
+            } else if (sphereBoard.speed_y < -1) {
                 sphereBoard.speed_y = -1;
             }
 
--- tagboard.js
+++ tagboard.js
@@ -2,15 +2,20 @@
 var l_tag_sprites = [];
 var l_tagboard = null;
 var l_canvas = null;
+/*global window*/
+/*global document*/
+/*global setInterval*/
+/*global SphereBoard*/
 
-function Options()
-{ 
+function Options() {
+    "use strict";
     this.bgcolor = '#FFFFFF';
     this.transparency = false;
 }
 
 var Sprite = {
-    createNew:function(name, url)  {
+    createNew: function (name, url) {
+        "use strict";
         var sprite = {};
 
         sprite.name = name;
@@ -25,8 +30,8 @@ var Sprite = {
 };
 
 var TagBoard = {
-
-    createNew:function(canvas, context, sprites, opts) {
+    createNew : function (canvas, context, sprites, opts) {
+        "use strict";
         var tagBoard = {};
 
         tagBoard.canvas = canvas;
@@ -46,83 +51,81 @@ var TagBoard = {
         tagBoard.frame_cnt = 0;
         tagBoard.start = (new Date()).getTime();
 
-        tagBoard.rotateX = function(point, radians) {
-            var y = point.y - tagBoard.height/2;
-            var cos_r = Math.cos(radians);
-            var sin_r = Math.sin(radians);
+        tagBoard.rotateX = function (point, radians) {
+            var y = point.y - tagBoard.height / 2,
+                cos_r = Math.cos(radians),
+                sin_r = Math.sin(radians);
 
             point.y = (y * cos_r) + (point.z * sin_r * -1.0) +
-                tagBoard.height/2;
+                tagBoard.height / 2;
             point.z = (y * sin_r) + (point.z * cos_r);
         };
 
 
-        tagBoard.rotateY = function(point, radians) {
-            var x = point.x - tagBoard.width/2;
-            var cos_r = Math.cos(radians);
-            var sin_r = Math.sin(radians);
+        tagBoard.rotateY = function (point, radians) {
+            var x = point.x - tagBoard.width / 2,
+                cos_r = Math.cos(radians),
+                sin_r = Math.sin(radians);
 
             point.x = (x * cos_r) + (point.z * sin_r * -1.0) +
-                tagBoard.width/2;
+                tagBoard.width / 2;
             point.z = (x * sin_r) + (point.z * cos_r);
         };
 
-        tagBoard.rotateZ = function(point, radians) {
-            var x = point.x - tagBoard.width/2;
-            var cos_r = Math.cos(radians);
-            var sin_r = Math.sin(radians);
+        tagBoard.rotateZ = function (point, radians) {
+            var x = point.x - tagBoard.width / 2,
+                cos_r = Math.cos(radians),
+                sin_r = Math.sin(radians);
 
-            point.y -= tagBoard.height/2;
-            point.x = (x * cos_r) + (point.y * sin_r * -1.0) +
-                tagBoard.width/2; 
-            point.y = (x * sin_r) + (point.y * cos_r) +
-                tagBoard.height/2;
+            point.y -= tagBoard.height / 2;
+            point.x = (x * cos_r) + (point.y * sin_r * -1.0) + tagBoard.width / 2;
+            point.y = (x * sin_r) + (point.y * cos_r) + tagBoard.height / 2;
         };
 
-        tagBoard.onMouseMove = function(x, y) {
+        tagBoard.onMouseMove = function (x, y) {
             tagBoard.mouse_x = x;
             tagBoard.mouse_y = y;
         };
 
 
-        tagBoard.onMouseClick = function(x, y) { 
-            if(tagBoard.focusspr !== null) { 
+        tagBoard.onMouseClick = function (x, y) {
+            if (tagBoard.focusspr !== null) {
                 window.location = tagBoard.focusspr.link;
             }
         };
 
-        tagBoard.spritesMove = function() { 
+        tagBoard.spritesMove = function () {
             //Sprites move!
         };
 
 
-        tagBoard.draw = function() { 
-            if(tagBoard.transparency) { 
+        tagBoard.draw = function () {
+            if (tagBoard.transparency) {
                 tagBoard.context.globalAlpha = 0.0;
-            } else { 
+            } else {
                 tagBoard.context.globalAlpha = 1;
             }
 
             tagBoard.context.fillStyle = tagBoard.bgcolor;
-            //tagBoard.context.fillRect(0, 0, tagBoard.width, tagBoard.height); 
-            tagBoard.context.clearRect(0, 0, tagBoard.width, tagBoard.height); 
-            tagBoard.context.fillRect(0, 0, tagBoard.width, tagBoard.height); 
+            //tagBoard.context.fillRect(0, 0, tagBoard.width, tagBoard.height);
+            tagBoard.context.clearRect(0, 0, tagBoard.width, tagBoard.height);
+            tagBoard.context.fillRect(0, 0, tagBoard.width, tagBoard.height);
             tagBoard.context.globalAlpha = 1;
 
-            if(tagBoard.debug) { 
-                var diff;
-                var fps;
-                tagBoard.frame_cnt++;
+            if (tagBoard.debug) {
+                var diff,
+                    fps;
+                tagBoard.frame_cnt += 1;
                 diff = (new Date()).getTime() - tagBoard.start;
                 fps = tagBoard.frame_cnt / (diff / 1000);
                 tagBoard.context.fillText(fps.toFixed(2), 30, 20);
             }
         };
 
-        tagBoard.drawSprite = function(sprite, font_size, style, is_focus, fast_draw) { 
+        tagBoard.drawSprite = function (sprite, font_size, style, is_focus, fast_draw) {
 
             //If fast_draw is true then skip setting font. It would cost more time on firefox.
-            if(!fast_draw) { 
+            if (!fast_draw) {
                 tagBoard.context.font = font_size.toString() + "px sans-serif";
             }
             tagBoard.context.fillStyle = style;
@@ -130,30 +133,28 @@ var TagBoard = {
             tagBoard.context.fillText(sprite.name, sprite.x, sprite.y);
 
             //TODO: We should compile the sprite and focusspr. is_focus might not be needed.
-            if(tagBoard.focusspr == sprite)
-            { 
-                w = tagBoard.context.measureText(sprite.name).width;
-                tagBoard.context.strokeRect(Math.max(sprite.x - w/2, 0),
-                sprite.y - font_size,
-                w, font_size+1);
+            if (tagBoard.focusspr === sprite) {
+                var w = tagBoard.context.measureText(sprite.name).width;
+                tagBoard.context.strokeRect(Math.max(sprite.x - w / 2, 0),
+                    sprite.y - font_size,
+                    w, font_size + 1);
             }
         };
 
-        tagBoard.render = function() 
-        {
+        tagBoard.render = function () {
             tagBoard.draw();
             tagBoard.spritesMove();
         };
 
 
-        tagBoard.mouseOnSprite = function(spr, fontsize) { 
+        tagBoard.mouseOnSprite = function (spr, fontsize) {
             var width;
 
             width = tagBoard.context.measureText(spr.name).width;
             //textAlign center
-            if(tagBoard.mouse_x >= Math.max(spr.x - width/2, 0) &&
-            tagBoard.mouse_x <= Math.min(spr.x + width/2, tagBoard.width) &&
-            tagBoard.mouse_y >= (spr.y - fontsize) && tagBoard.mouse_y <= spr.y) { 
+            if (tagBoard.mouse_x >= Math.max(spr.x - width / 2, 0) &&
+                    tagBoard.mouse_x <= Math.min(spr.x + width / 2, tagBoard.width) &&
+                    tagBoard.mouse_y >= (spr.y - fontsize) && tagBoard.mouse_y <= spr.y) {
                 return true;
             }
 
@@ -161,9 +162,9 @@ var TagBoard = {
         };
 
 
-        tagBoard.sortSprites = function(member, reverse) { 
-            sort_function = function(a, b) { 
-                if(reverse) { 
+        tagBoard.sortSprites = function (member, reverse) {
+            var sort_function = function (a, b) {
+                if (reverse) {
                     return (b[member] - a[member]);
                 }
 
@@ -178,55 +179,58 @@ var TagBoard = {
 };
 
 //Call this function after the fillStyle.
-function createTagBoard(type, opts)
-{ 
-    var context;
-    var obj = null;
+function createTagBoard(type, opts) {
+    "use strict";
+    var context,
+        obj = null;
     l_canvas = document.getElementById("tagboardCanvas");
     context = l_canvas.getContext("2d");
 
-    switch(type){ 
-        case "sphere":
-            obj = SphereBoard.createNew(l_canvas, context, l_tag_sprites, opts);
-            break;
-        case "matrix":
-            break; 
-        default:
-            break;
+    switch (type) {
+    case "sphere":
+        obj = SphereBoard.createNew(l_canvas, context, l_tag_sprites, opts);
+        break;
+    case "matrix":
+        break;
+    default:
+        break;
     }
 
     l_tagboard = obj;
 }
 
-function addTag(name, url) 
-{ 
+function addTag(name, url) {
+    "use strict";
     l_tag_sprites[l_tag_sprites.length] = Sprite.createNew(name, url);
 }
 
-function timeout() { 
+function timeout() {
+    "use strict";
     l_tagboard.render();
 }
 
 function onMouseMove(ev) {
-    var x = ev.pageX - l_canvas.offsetLeft;
-    var y = ev.pageY - l_canvas.offsetTop;
+    "use strict";
+    var x = ev.pageX - l_canvas.offsetLeft,
+        y = ev.pageY - l_canvas.offsetTop;
 
     l_tagboard.onMouseMove(x, y);
 }
 
-function onMouseClick(ev){ 
-    var x = ev.pageX - l_canvas.offsetLeft;
-    var y = ev.pageY - l_canvas.offsetTop;
+function onMouseClick(ev) {
+    "use strict";
+    var x = ev.pageX - l_canvas.offsetLeft,
+        y = ev.pageY - l_canvas.offsetTop;
 
     l_tagboard.onMouseClick(x, y);
 }
 
-function start()
-{ 
+function start() {
+    "use strict";
     l_canvas.addEventListener("mousemove", onMouseMove, false);
     l_canvas.addEventListener("click", onMouseClick, false);
 
-    setInterval(timeout, 1000/20);
+    setInterval(timeout, 1000 / 20);
 }
 
 
